05ac50fb9ca2e8c46d0ce24842af1830d65ae50f
ievo <xlievo@live.com>
2023-01-19 16:07:32 +0800

    update: UnitTest

--- Business.SourceGenerator.UnitTest/UnitTest1.cs
+++ Business.SourceGenerator.UnitTest/UnitTest1.cs
@@ -275,6 +275,8 @@ namespace UnitAssembly
     }}
 }}
 ";
+            typeof(MyCode.ClassGeneric<string>)
+                .GetGenericType<int>().CreateInstance(params object[] args);
 
             var path = System.IO.Path.Combine(AppContext.BaseDirectory, "TestTemp", file);
 
