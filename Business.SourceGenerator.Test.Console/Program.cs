a5d06f9880233b80e943bc7fadc3ccebb5925693
ievo <xlievo@live.com>
2023-02-03 17:38:13 +0800

    add: Business.SourceGenerator.Test.Console

---
+++ Business.SourceGenerator.Test.Console/Program.cs
@@ -0,0 +1,106 @@
+﻿using Business.SourceGenerator;
+using Business.SourceGenerator.Meta;
+using Business.SourceGenerator.Test;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+//using System.Text.Json;
+//using System.Text.Json.Serialization;
+//using System.Text.Json.Serialization.Metadata;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Business.SourceGenerator.Test.Console
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var result = typeof(MyCode.ClassGeneric<string>)
+                    .CreateInstance<IGeneratorAccessor>()
+                    .AccessorSet<IGeneratorAccessor>("A", "WWW")
+                    .AccessorSet<IGeneratorAccessor>("B", new Dictionary<string, string>());
+
+            var MyStruct111 = typeof(MyStruct)
+                .CreateInstance<IGeneratorAccessor>("aaa111")
+                .AccessorSet<IGeneratorAccessor>("bbb", 888)
+                .AccessorSet<IGeneratorAccessor>("ccc", DateTimeOffset.Now);
+
+            if (MyStruct111.AccessorGet("StructMember2", out (int c1, string c2) value222, "a", default, (777, "xzzzxx")))
+            {
+                System.Console.WriteLine(value222);
+            }
+
+            if (MyStruct111.AccessorGet("StructMethod7", out ValueTask<(int c1, string c2)> value7, "a", default, (888, "xzzzxx")))
+            {
+                System.Console.WriteLine(await value7);
+            }
+
+            var value777 = await MyStruct111.AccessorGetAsync<(int c1, string c2)>("StructMethod7", "a", default, (999, "xzzzxx"));
+
+            System.Console.WriteLine(value777);
+
+            if (MyStruct111.AccessorGet("aaa", out string value))
+            {
+                System.Console.WriteLine(value);
+            }
+
+            if (MyStruct111.AccessorGet("bbb", out int? value22))
+            {
+                System.Console.WriteLine(value22);
+            }
+
+            if (MyStruct111.AccessorGet("ccc", out DateTimeOffset? value3))
+            {
+                System.Console.WriteLine(value3);
+            }
+
+            var value2 = MyStruct111.AccessorGetAsync("ccc");
+
+            var data = typeof(ResultObject<>).GetGenericType(typeof(MyStruct)).CreateInstance(MyStruct111, 8888, "message", true);
+
+            var data5 = typeof(ResultObject3<>).GetGenericType<MyStruct5>().CreateInstance<ResultObject3<MyStruct5>>(new MyStruct5("```5555555555555555555555~!@#"), 333, "message", true);
+
+            var data6 = typeof(ResultObject3<>).GetGenericType(typeof(MyStruct6)).CreateInstance<ResultObject3<MyStruct6>>(new MyStruct6("```6666666666666666666666~!@#"), 333, "message", true);
+
+            var data7 = typeof(ResultObject3<>).GetGenericType(typeof(MyStruct7)).CreateInstance<ResultObject3<MyStruct7>>(new MyStruct7("```7777777777777777777777~!@#"), 333, "message", true);
+
+            var data8 = typeof(ResultObject3<>).GetGenericType(typeof(MyStruct8)).CreateInstance<ResultObject3<MyStruct8>>(new MyStruct8("```8888888888888888888888~!@#"), 333, "message", true);
+
+            var data9 = typeof(ResultObject3<>).GetGenericType(typeof(MyStruct9)).CreateInstance<ResultObject3<MyStruct9>>(new MyStruct9("```9999999999999999999999~!@#"), 333, "message", true);
+
+            var data10 = typeof(ResultObject3<>).GetGenericType(typeof(MyStruct10)).CreateInstance<ResultObject3<MyStruct10>>(new MyStruct10("```1010101010101010101~!@#"), 333, "message", true);
+
+            System.Console.WriteLine(data);
+
+            System.Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(data5, typeof(ResultObject3<MyStruct5>), SourceGenerationContext2.Default));
+            System.Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(data6, typeof(ResultObject3<MyStruct6>), SourceGenerationContext2.Default));
+            System.Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(data7, typeof(ResultObject3<MyStruct7>), SourceGenerationContext2.Default));
+            System.Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(data8, typeof(ResultObject3<MyStruct8>), SourceGenerationContext2.Default));
+            System.Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(data9, typeof(ResultObject3<MyStruct9>), SourceGenerationContext2.Default));
+            System.Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(data10, typeof(ResultObject3<MyStruct10>), SourceGenerationContext2.Default));
+
+            System.Console.WriteLine(System.AppContext.BaseDirectory);
+
+            System.Console.Read();
+
+            return 0;
+        }
+    }
+}
+
+[JsonSourceGenerationOptions(WriteIndented = true)]
+//[JsonSerializable(typeof(ResultObject<MyStruct>), GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSerializable(typeof(ResultObject2<MyStruct2>), GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSerializable(typeof(ResultObject3<MyStruct3>), GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSerializable(typeof(ResultObject4<MyStruct4>), GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSerializable(typeof(ResultObject3<MyStruct5>), GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSerializable(typeof(ResultObject3<MyStruct6>), GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSerializable(typeof(ResultObject3<MyStruct7>), GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSerializable(typeof(ResultObject3<MyStruct8>), GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSerializable(typeof(ResultObject3<MyStruct9>), GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSerializable(typeof(ResultObject3<MyStruct10>), GenerationMode = JsonSourceGenerationMode.Metadata)]
+internal partial class SourceGenerationContext2 : JsonSerializerContext { }
---
+++ Business.SourceGenerator.Test.Console/Structs.cs
@@ -0,0 +1,244 @@
+﻿using Business.SourceGenerator.Test;
+using System;
+using System.Threading.Tasks;
+
+public struct ResultObject4<Type> : IResult2<Type>
+{
+    /// <summary>
+    /// Activator.CreateInstance
+    /// </summary>
+    /// <param name="dataType"></param>
+    /// <param name="data"></param>
+    /// <param name="state"></param>
+    /// <param name="message"></param>
+    /// <param name="callback"></param>
+    /// <param name="genericDefinition"></param>
+    /// <param name="checkData"></param>
+    /// <param name="hasData"></param>
+    /// <param name="hasDataResult"></param>
+    public ResultObject4(System.Type dataType, Type data, int state = 1, string message = null, string callback = null, System.Type genericDefinition = null, bool checkData = true, bool hasData = false, bool hasDataResult = false)
+    {
+        this.DataType = dataType;
+        this.Data = data;
+        this.State = state;
+        this.Message = message;
+        this.HasData = checkData ? !Equals(null, data) : hasData;
+
+        this.Callback = callback;
+        this.GenericDefinition = genericDefinition;
+        this.HasDataResult = hasDataResult;
+    }
+
+    /// <summary>
+    /// MessagePack.MessagePackSerializer.Serialize(this)
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="state"></param>
+    /// <param name="message"></param>
+    /// <param name="hasData"></param>
+    public ResultObject4(Type data, int state, string message, bool hasData)
+    {
+        this.Data = data;
+        this.State = state;
+        this.Message = message;
+        this.HasData = hasData;
+
+        this.Callback = null;
+        this.DataType = null;
+        this.GenericDefinition = null;
+        this.HasDataResult = false;
+    }
+
+    /// <summary>
+    /// The results of the state is greater than or equal to 1: success, equal to 0: system level exceptions, less than 0: business class error.
+    /// </summary>
+    public int State { get; }
+
+    /// <summary>
+    /// Success can be null
+    /// </summary>
+    public string Message { get; }
+
+    /// <summary>
+    /// Specific dynamic data objects
+    /// </summary>
+    dynamic Business.SourceGenerator.Test.IResult.Data { get => Data; }
+
+    /// <summary>
+    /// Specific Byte/Json data objects
+    /// </summary>
+    public Type Data { get; set; }
+
+    /// <summary>
+    /// Whether there is value
+    /// </summary>
+    public bool HasData { get; }
+
+    /// <summary>
+    /// Gets the token of this result, used for callback
+    /// </summary>
+    public string Callback { get; set; }
+
+    /// <summary>
+    /// Data type
+    /// </summary>
+    public System.Type DataType { get; }
+
+    /// <summary>
+    /// GenericDefinition
+    /// </summary>
+    public System.Type GenericDefinition { get; }
+
+    /// <summary>
+    /// Return data or not
+    /// </summary>
+    public bool HasDataResult { get; }
+
+    ///// <summary>
+    ///// Json format
+    ///// </summary>
+    ///// <returns></returns>
+    //public override string ToString() => Business.Core.Utils.Help.JsonSerialize(this);
+
+    /// <summary>
+    /// ProtoBuf,MessagePack or Other
+    /// </summary>
+    /// <param name="dataBytes"></param>
+    /// <returns></returns>
+    public byte[] ToBytes(bool dataBytes = true) => throw new NotImplementedException(); //Utils.Help.ProtoBufSerialize(this);
+}
+
+public partial struct MyStruct
+{
+    public string? aaa { get; set; }
+
+    public int? bbb { get; set; }
+
+    public DateTimeOffset? ccc { get; set; }
+
+    public MyStruct(string aaa)
+    {
+        this.aaa = aaa ?? throw new ArgumentNullException(nameof(aaa));
+    }
+
+    public (int c1, string c2) StructMember2(ref string? a, out int? b, ref (int c1, string c2) c, out (int? c1, string? c2) d)
+    {
+        d = default;
+        var dd = c.c1 as dynamic;
+
+        b = 9;
+
+        return c;
+    }
+
+    public ValueTask<(int c1, string c2)> StructMethod7(ref string? a, out int? b, ref (int c1, string c2) c, out (int? c1, string? c2) d)
+    {
+        d = default;
+        var dd = c.c1 as dynamic;
+
+        //this.A = a;
+        b = 9;
+
+        return ValueTask.FromResult(c);
+    }
+}
+
+public partial struct MyStruct2
+{
+    public string aaa { get; set; }
+
+    public MyStruct2(string aaa)
+    {
+        this.aaa = aaa ?? throw new ArgumentNullException(nameof(aaa));
+    }
+}
+
+public partial struct MyStruct3
+{
+    public string aaa { get; set; }
+
+    public MyStruct3(string aaa)
+    {
+        this.aaa = aaa ?? throw new ArgumentNullException(nameof(aaa));
+    }
+}
+
+public partial struct MyStruct4
+{
+    public string aaa { get; set; }
+
+    public MyStruct4(string aaa)
+    {
+        this.aaa = aaa ?? throw new ArgumentNullException(nameof(aaa));
+    }
+}
+
+public partial struct MyStruct5
+{
+    public string aaa { get; set; }
+
+    public MyStruct5(string aaa)
+    {
+        this.aaa = aaa ?? throw new ArgumentNullException(nameof(aaa));
+    }
+}
+
+public partial struct MyStruct6
+{
+    public string aaa { get; set; }
+
+    public MyStruct6(string aaa)
+    {
+        this.aaa = aaa ?? throw new ArgumentNullException(nameof(aaa));
+    }
+}
+
+public partial struct MyStruct7
+{
+    public string aaa { get; set; }
+
+    public MyStruct7(string aaa)
+    {
+        this.aaa = aaa ?? throw new ArgumentNullException(nameof(aaa));
+    }
+}
+
+public partial struct MyStruct8
+{
+    public string aaa { get; set; }
+
+    public MyStruct8(string aaa)
+    {
+        this.aaa = aaa ?? throw new ArgumentNullException(nameof(aaa));
+    }
+}
+
+public partial struct MyStruct9
+{
+    public string aaa { get; set; }
+
+    public MyStruct9(string aaa)
+    {
+        this.aaa = aaa ?? throw new ArgumentNullException(nameof(aaa));
+    }
+}
+
+public partial struct MyStruct10
+{
+    public string aaa { get; set; }
+
+    public MyStruct10(string aaa)
+    {
+        this.aaa = aaa ?? throw new ArgumentNullException(nameof(aaa));
+    }
+}
+
+namespace MyCode
+{
+    public partial class ClassGeneric<T> : System.Collections.Generic.Dictionary<T, T>
+    {
+        public T A { get; set; }
+
+        public System.Collections.Generic.Dictionary<T, T> B { get; set; }
+    }
+}
