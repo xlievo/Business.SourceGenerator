e2880ddc15e2f25039f9fda7671ef8b289235802
ievo <xlievo@live.com>
2023-05-13 00:50:58 +0800

    update: Optimize Meta.

--- Business.SourceGenerator.UnitTest/TestTemp/TypeInfo.cs
+++ Business.SourceGenerator.UnitTest/TestTemp/TypeInfo.cs
@@ -4,12 +4,18 @@
 
 
 
+using System.Collections.Generic;
+
 namespace MyCode
 {
     public partial struct TypeInfo<Type>
     {
         public System.Threading.Tasks.ValueTask<T> Test2<T>(T? a, System.DateTimeOffset? c, Type d, dynamic e)
         {
+            var t = new Dictionary<T, Type>();
+
+            var type = t.GetType();
+
             return System.Threading.Tasks.ValueTask.FromResult<T>(default);
         }
     }
